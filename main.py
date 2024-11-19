f0e05fddb8e6f793aece21e1825a57422ee4e8c3
Alex <125302863+alexky0@users.noreply.github.com>
2024-11-19 15:10:12 -0500

    high cholesterol

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("hello world")
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+df = pd.read_csv('medical_examination.csv')
+
+df['bmi'] = df['weight']/((df['height']/100)**2)
+df['overweight'] = np.where(df['bmi'] > 25, 1, 0)
+df['cholesterol'] = np.where(df['cholesterol'] >= 1, 1, 0)
+df['gluc'] = np.where(df['gluc'] >= 1, 1, 0)
 
 
-if __name__ == "__main__":
-    main()
